d6284fbe022f549080def9082b5776975706e70d
SMB <stephenmbassett777@gmail.com>
2023-02-19 17:22:03 -0500

    Added header to account setup stepper

--- src/app/feature/account-setup/components/account-setup/account-setup.component.ts
+++ src/app/feature/account-setup/components/account-setup/account-setup.component.ts
@@ -1,38 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
-
+import { Component, OnInit } from "@angular/core";
+import { MenuItem } from "primeng/api";
 
 @Component({
-  selector: 'vvu-account-setup',
-  templateUrl: './account-setup.component.html',
-  styleUrls: ['./account-setup.component.scss']
+  selector: "vvu-account-setup",
+  templateUrl: "./account-setup.component.html",
+  styleUrls: ["./account-setup.component.scss"],
 })
 export class AccountSetupComponent implements OnInit {
+  items!: MenuItem[];
 
-  items!: MenuItem[]
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-
     this.items = [
       {
-        label: 'Choose Account Type',
-        routerLink: 'account-type'
+        label: "Account Type",
+        routerLink: "account-type",
       },
       {
-        label: 'Admin Info',
-        routerLink: 'admin-info'
+        label: "Admin Info",
+        routerLink: "admin-info",
       },
       {
-        label: 'Role Selection',
-        routerLink: 'account-roles'
+        label: "Admin Roles",
+        routerLink: "account-roles",
       },
       {
-        label: 'Company Signup',
-        routerLink: 'company-verification'
-      }
-    ]
+        label: "Company Info",
+        routerLink: "company-verification",
+      },
+    ];
   }
-
 }
